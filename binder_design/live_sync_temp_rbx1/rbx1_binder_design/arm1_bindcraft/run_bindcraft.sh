30aee22542fa5eabc06674ff4d79e9c982ca1c14
QntmSeer <qntmqrks@gmail.com>
2026-03-11 19:23:24 +0100

    Cleanup: Remove redundant scripts, logs, and temporary sync backups

--- binder_design/live_sync_temp_rbx1/rbx1_binder_design/arm1_bindcraft/run_bindcraft.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-eval "$(conda shell.bash hook)"
-conda activate bindcraft
-export LD_LIBRARY_PATH="/home/shadeform/miniconda3/envs/bindcraft/lib/python3.10/site-packages/nvidia/cudnn/lib:$LD_LIBRARY_PATH"
-export XLA_FLAGS="--xla_gpu_cuda_data_dir=/usr/local/cuda"
-export TF_FORCE_GPU_ALLOW_GROWTH=true
-cd ~/rbx1_binder_design/BindCraft
-python bindcraft.py --settings ../arm1_bindcraft/rbx1_config.json
